6fc56bebd52809baa64ee1fe63257b0c2619b9c4
Rudy Sanchez <texas697@gmail.com>
2020-01-28 12:16:44 -0600

    forgot some semi-colons

--- src/containers/Chart/selectors.js
+++ src/containers/Chart/selectors.js
@@ -1,24 +1,24 @@
 
 export const getSelectedMetrics = state => {
-    return state.metrics.selectedMetrics
+    return state.metrics.selectedMetrics;
 };
 
 
 export const getSeriesData = state => {
-    return state.measurements.seriesData
+    return state.measurements.seriesData;
 };
 
 
 export const getAxisData = state => {
-    return state.measurements.axisData
+    return state.measurements.axisData;
 };
 
 
 export const getTrafficSeriesData = state => {
-    return state.measurements.trafficSeriesData
+    return state.measurements.trafficSeriesData;
 };
 
 
 export const getWindowHeight = state => {
-    return state.measurements.windowHeight
+    return state.measurements.windowHeight;
 };
